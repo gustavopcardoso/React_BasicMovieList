ce5c2aba22beb06bdc3effb8f14f6296dd92a5e8
gustavo.cardoso <gustavo.pc.ccm@gmail.com>
2021-05-13 23:21:18 -0300

    Desafio Rocketseat#2: Refatoração da aplicação para componentizar as funcionalidades de listagem de gênero dos filmes e exibição dos filmes do gênero selecionado.

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,47 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+/* By convention, interfaces for typing props are named following the structure "Component name" + "Props". */
+interface SideBarProps {
+  genreId: number;
+
+  /* Declaring functions types within interfaces: https://medium.com/@jeffbutsch/typescript-interface-functions-c691a108e3f1 */
+  onGenreChange(id: number): void;
+}
+export function SideBar(props: SideBarProps) {
+
+  const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+
+  useEffect(() => {
+    api.get<GenreResponseProps[]>('genres').then(response => {
+      setGenres(response.data);
+    });
+  }, []);
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            /* Passing parameters back to the parent component: https://reactjs.org/docs/faq-functions.html */
+            onClick={() => { props.onGenreChange(genre.id) }}
+            selected={props.genreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  );
 }
